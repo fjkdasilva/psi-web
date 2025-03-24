718668be0d4d4b13d3fe4dce2e9984c86bf3f915
Flavio J.K. da Silva <fjkdasilva@gmail.com>
2025-03-24 10:14:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO, emit, join_room
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*', engineio_logger=True)
 
-rooms = {}  # {room: {'data': {'trials': [], 'current_trial': 0}, 'clients': {sid: role}}}
+rooms = {}  # {room: {'data': {'trials': [], 'current_trial': 0, 'max_trials': 10}, 'clients': {sid: role}}}
 
 @app.route('/')
 def index():
@@ -13,7 +13,7 @@ def index():
     if role not in ['transmitter', 'receiver']:
         return "Invalid role. Use ?role=transmitter or ?role=receiver", 400
     if room not in rooms:
-        rooms[room] = {'data': {'trials': [], 'current_trial': 0}, 'clients': {}}
+        rooms[room] = {'data': {'trials': [], 'current_trial': 0, 'max_trials': 10}, 'clients': {}}
     print(f"Server: Serving {role} in room {room}")
     return render_template('index.html', role=role, room=room)
 
@@ -30,7 +30,7 @@ def handle_join_room(data):
     sid = request.sid
     print(f"Server: {role} ({sid}) joining room {room}")
     if room not in rooms:
-        rooms[room] = {'data': {'trials': [], 'current_trial': 0}, 'clients': {}}
+        rooms[room] = {'data': {'trials': [], 'current_trial': 0, 'max_trials': 10}, 'clients': {}}
     
     # Add client to room with role
     join_room(room)
@@ -44,17 +44,47 @@ def handle_join_room(data):
     print(f"Server: {len(clients)} clients in room {room}: {list(clients.keys())} "
           f"(T: {len(transmitters)}, R: {len(receivers)})")
     
-    # If we have at least one unpaired transmitter and receiver, pair them and start a trial
-    if transmitters and receivers and rooms[room]['data']['current_trial'] < len(transmitters):
-        trial = rooms[room]['data']['current_trial'] + 1
-        t_sid = transmitters[trial - 1]  # Pair with the next available transmitter
-        r_sid = receivers[trial - 1]     # Pair with the next available receiver
+    # Start trials if we have at least one pair
+    if len(transmitters) >= 1 and len(receivers) >= 1 and rooms[room]['data']['current_trial'] == 0:
+        start_next_trial(room, transmitters[0], receivers[0])
+
+@socketio.on('submit_result')
+def handle_result(data):
+    room = data['room']
+    trial = data['trial']
+    transmitter_symbol = data.get('transmitter_symbol')  # Sent by transmitter
+    receiver_symbol = data.get('receiver_symbol')       # Sent by receiver
+    sid = request.sid
+    role = rooms[room]['clients'][sid]
+    
+    # Store result
+    trial_data = next((t for t in rooms[room]['data']['trials'] if t['trial'] == trial), None)
+    if not trial_data:
+        trial_data = {'trial': trial, 'transmitter_symbol': None, 'receiver_symbol': None}
+        rooms[room]['data']['trials'].append(trial_data)
+    
+    if role == 'transmitter':
+        trial_data['transmitter_symbol'] = transmitter_symbol
+    elif role == 'receiver':
+        trial_data['receiver_symbol'] = receiver_symbol
+    
+    # If both results are in, move to next trial
+    if trial_data['transmitter_symbol'] is not None and trial_data['receiver_symbol'] is not None:
+        print(f"Server: Trial {trial} complete: T={trial_data['transmitter_symbol']}, "
+              f"R={trial_data['receiver_symbol']}, Correct={trial_data['transmitter_symbol'] == trial_data['receiver_symbol']}")
+        start_next_trial(room, rooms[room]['clients'].keys()[0], rooms[room]['clients'].keys()[1])
+
+def start_next_trial(room, t_sid, r_sid):
+    current = rooms[room]['data']['current_trial']
+    max_trials = rooms[room]['data']['max_trials']
+    if current < max_trials:
+        trial = current + 1
+        rooms[room]['data']['current_trial'] = trial
         print(f"Server: Starting trial {trial} for pair {t_sid} (T) and {r_sid} (R)")
         emit('start_trial', {'trial': trial, 'pair': [t_sid, r_sid]}, room=room)
-        rooms[room]['data']['current_trial'] = trial
     else:
-        print(f"Server: Not starting trial - T: {len(transmitters)}, R: {len(receivers)}, "
-              f"trial {rooms[room]['data']['current_trial']}")
+        print(f"Server: All {max_trials} trials complete for room {room}")
+        emit('trials_complete', {'results': rooms[room]['data']['trials']}, room=room)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
